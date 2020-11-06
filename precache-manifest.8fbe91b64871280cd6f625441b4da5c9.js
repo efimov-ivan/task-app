b456d20b85b42c3990b094c52b1a76dadafa119c
I.Efimov <i.efimov.jamteam@gmail.com>
2020-11-07 00:08:14 +0600

    Updates

---
+++ precache-manifest.8fbe91b64871280cd6f625441b4da5c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f70f3cd5aa11753340a3de8cd1d4bd5b",
+    "url": "/task-app/index.html"
+  },
+  {
+    "revision": "1aef63b3e083209c8d8a",
+    "url": "/task-app/static/css/2.260df227.chunk.css"
+  },
+  {
+    "revision": "c892b3bbee8dd4feec7a",
+    "url": "/task-app/static/css/main.7152a3da.chunk.css"
+  },
+  {
+    "revision": "1aef63b3e083209c8d8a",
+    "url": "/task-app/static/js/2.e0428312.chunk.js"
+  },
+  {
+    "revision": "b29b299617ffa402b87d674bd1ecf33e",
+    "url": "/task-app/static/js/2.e0428312.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c892b3bbee8dd4feec7a",
+    "url": "/task-app/static/js/main.469a86e0.chunk.js"
+  },
+  {
+    "revision": "f3a2472f8d47fdcd861d",
+    "url": "/task-app/static/js/runtime-main.eda20655.js"
+  }
+]);
